50d899fd87342825461cc016af4c611f1fc5bc7b
Vismaya Wilson <vismayawilson3@gmail.com>
2022-04-11 12:11:08 +0400

    Initial react code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const recipieObj = [
+    {
+    id:"1",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+  {
+    id:"1",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+  {
+    id:"2",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+  {
+    id:"3",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+  {
+    id:"4",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+  {
+    id:"5",
+    title:"Spaghetti",
+    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
+  },
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App recipie-list-container">
+      {recipieObj.map((recipie,index) => 
+      <Recipie recipieObj={recipie}/>)}
+      
     </div>
   );
 }
 
+
+function Recipie({recipieObj}){
+  
+  return(<div>
+    <div className="recipie-container">
+      <img src={recipieObj.picture} alt="" className="recipe-image" />
+      <p className="recipie-name">{recipieObj.title}</p>
+    </div>
+  </div>)
+}
 export default App;
