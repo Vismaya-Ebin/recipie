3563aa3a35496b67fba87b3bddcda2cbb78e45c6
Vismaya Wilson <vismayawilson3@gmail.com>
2022-04-13 12:27:14 +0400

    Data moved to BE

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
-
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { recipieAPI } from "./helper";
+import {useEffect} from "react";
 
 function App() {
-  const recipieObj = [
-    {
-    id:"1",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-  {
-    id:"1",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-  {
-    id:"2",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-  {
-    id:"3",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-  {
-    id:"4",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-  {
-    id:"5",
-    title:"Spaghetti",
-    picture:"https://media.istockphoto.com/photos/spaghetti-pasta-with-meatballs-picture-id1298256576"
-  },
-];
+  const [recipieObj, setRecipieObj] = useState([]);
+  
+const getAllRecipies = () =>{
+  fetch(`${recipieAPI}/recipie`)
+    .then((res) => res.json())
+    .then((data) => {
+      setRecipieObj(data);
+    });
+}
+   //To call APi only for one time we have to use useEffect with []
+  useEffect((getAllRecipies),[])
   return (
     <div className="App recipie-list-container">
-      {recipieObj.map((recipie,index) => 
-      <Recipie recipieObj={recipie}/>)}
-      
+      {recipieObj.map((recipie, index) => (
+        <Recipie recipieObj={recipie} />
+      ))}
     </div>
   );
 }
 
-
-function Recipie({recipieObj}){
-  
-  return(<div>
-    <div className="recipie-container">
-      <img src={recipieObj.picture} alt="" className="recipe-image" />
-      <p className="recipie-name">{recipieObj.title}</p>
+function Recipie({ recipieObj }) {
+  return (
+    <div>
+      <div className="recipie-container">
+        <img src={recipieObj.picture} alt="" className="recipe-image" />
+        <p className="recipie-name">{recipieObj.title}</p>
+      </div>
     </div>
-  </div>)
+  );
 }
 export default App;
